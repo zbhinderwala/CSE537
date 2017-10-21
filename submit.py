f938b33949a717265c62758444a2882e4766c7f6
zbhinderwala <32024389+zbhinderwala@users.noreply.github.com>
2017-10-20 23:45:44 -0400

    updated

--- submit.py
+++ submit.py
@@ -2,30 +2,168 @@
 #You are given L and M as input
 #Each of your functions should return the minimum possible L value alongside the marker positions
 #Or return -1,[] if no solution exists for the given L
-class Golumb_Ruler:
-    M = 0  # Marker Order
-    L = 0  # Length
 
-    def __init__(self, M, L):
+## Defined a class for Golomb_Ruler
+class Golomb_Ruler:
+    """
+    The Golomb ruler consists of M Markers whose total length is L
+    """
+    M = 0  # Markers
+    L = 0  # Length of ruler
+
+    def __init__(self, L, M):
         self.M = M
         self.L = L
 
+## Backtracking class with initialising the ruler ,domain and assignments
 class BackTracking:
     n = 0
     d = 0
-    c = 0  # number of constraints
-    counter = 0
-    consistent_dict = dict()
+    consistent_assignments = dict() # Keeping a track of all different assignments encountered
+
+    def __init__(self, ruler):
+        self.n = ruler.M
+        self.d = ruler.L + 1 # Domain size
+        # Domain values take all integers on the number line starting from 0 to L
+        self.domain = [x for x in range(self.d)]
+        self.assignment = dict() # Assignment dictionary
+        # Initially assign all Markers as None and the first one with 0
+        for i in range(self.n):
+            self.assignment[i] = None
+        self.assignment[0]=0
+
+    def complete(self,assignment):
+        """
+        Check if an assignment is complete and returns true or false
+        """
+        b = True
+        for i in range(self.n):
+            if assignment[i] is None:
+                b = False
+        return b
+
+
+    def select_unassigned_variable(self):
+        """
+        # Returns the first unassigned marker variable
+        :return: marker
+        """
+        for i in range(self.n):
+            if self.assignment[i] is None:
+                return i
+
+
+    def order_domain_values(self,marker,asignment):
+        """
+        # Return all the available domain values for a variable
+        :param var: Marker for which domain is sought
+        :param asignment:
+        :return:domain
+        """
+        return self.domain
+
+
+    def consistency(self,marker,value,assignment):
+        """
+        This method performs the folloing consistency checks
+        1. X1<X2<X3<...Xn where Xi is the ith Marker value
+        2. Distance between every pair of marker is unique
+        :param marker:
+        :param value:
+        :param assignment:
+        :return: True if assignment of value to marker is consistent else False
+        """
+        temp_assignment = {} # Dictionary to hold intermediate assignment
+        for i in range(len(assignment)):
+            if i == marker:
+                temp_assignment[i] = value # Assign marker the value
+            else:
+                temp_assignment[i]=assignment[i]
+
+        # If this assignment is already part of consistenct assignment return its status
+        if tuple(temp_assignment.items()) in self.consistent_assignments:
+            return self.consistent_assignments[tuple(temp_assignment.items())]
+
+        # Checking constraint  1 of values in increasing order
+        for i in range (1,len(temp_assignment)):
+            if temp_assignment[i]== None or temp_assignment[i-1]==None:
+                continue
+            if temp_assignment[i]<=temp_assignment[i-1]:
+                # Previous marker value is greater that current marker assignment then this is an inconsistent assignment return False
+                self.consistent_assignments[tuple(temp_assignment.items())] = False
+                return False
+
+        # Calculate all distance between assignment values
+        distance_list=[]
+        for i in range(self.n):
+            for j in range(self.n-1-i):
+                b=temp_assignment[self.n-1-i]
+                a=temp_assignment[(self.n-1-i)-(j+1)]
+
+                if b is None or a is None:
+                    continue
+                distance_list.append(b-a) # Append all distances of every marker values
+
+        distance_list.sort()
+        # If list has duplicate values means that distances are not unique and assignment is inconsistent
+        if len(distance_list) > len(set(distance_list)):
+            self.consistent_assignments[tuple(temp_assignment.items())]=False
+            return False
+        # Check if the distance of last marker value and first marker value is less than length of ruler then return False
+        if temp_assignment[self.n-1] is not None and temp_assignment[self.n-1]-temp_assignment[0]<self.d-1:
+            return False
+        # If none of the conistency checks are failed then return True
+        self.consistent_assignments[tuple(temp_assignment.items())]=True
+        return True
+
+# ForwardChecking class with its domain , variables and assignment
+class ForwardChecking:
+    n = 0
+    d = 0
+    consistent_assignments = dict()  # Keeping a track of all different assignments encountered
 
     def __init__(self, ruler):
         self.n = ruler.M
         self.d = ruler.L + 1
-        self.domain = [[1 for x in range(self.d)] for x in range(self.n)]
+        # For FC we will keep the domain as a dictionary of all values initially set to 0
+        self.domain = [[0 for x in range(self.d)] for x in range(self.n)]
         self.assignment = dict()
+        # Setting the initial domain list for every variable
+        for i in range(self.n):
+            if i == 0:
+                temp_list = [1 for x in range(self.d)]
+                temp_list[0] = 0
+                self.domain[i] = temp_list
+            elif i == self.n - 1:
+                temp_list = [1 for x in range(self.d)]
+                temp_list[self.d - 1] = 0
+                self.domain[i] = temp_list
+            else:
+                temp_list = [0 for x in range(self.d)]
+                for j in range(i + 1, self.n):
+                    temp_list[self.d - self.n + j] = 1
+                self.domain[i] = temp_list
+
+        # Initially assign all Markers as None and the first one with 0
         for i in range(self.n):
             self.assignment[i] = None
+        self.assignment[0]=0
 
-    def complete(self,assignment):
+    def copy_domain(self, domain1, domain2):
+        """
+        Copy the domain temporarily before modifying it
+        :param domain1:
+        :param domain2:
+        :return:
+        """
+        for i in range(len(domain2)):
+            for j in range(len(domain2[i])):
+                domain1[i][j] = domain2[i][j]
+
+    def complete(self, assignment):
+        """
+        Check if an assignment is complete and returns true or false
+        """
         b = True
         for i in range(self.n):
             if assignment[i] is None:
@@ -33,133 +171,207 @@ class BackTracking:
         return b
 
     def select_unassigned_variable(self):
+        """
+        Returns the first unassigned marker variable
+        :return: marker
+        """
         for i in range(self.n):
             if self.assignment[i] is None:
                 return i
 
 
-    def order_domain_values(self,var,asignment):
-        available_values = []
-        for i in range(len(self.domain[var])):
-            if self.domain[var][i] != 0:
-                available_values.append(i)
-        return available_values
-
-    def consistency(self,var,value,source_assignment):
-
-        # Creating a partial consistency with value
-        assignment = {}
-        for i in range(len(source_assignment)):
-            if i == var:
-                assignment[i] = value
-            else:
-                assignment[i]=source_assignment[i]
-
-        # If this assignment is already part of consistenct_dict return its value
-        if tuple(assignment.items()) in self.consistent_dict:
-            return self.consistent_dict[tuple(assignment.items())]
-
-        # Checking constraint  1 og i ncreasing order values
-        for i in range (1,len(assignment)):
-            if assignment[i]== None or assignment[i-1]==None:
+    def order_domain_values(self, var, assignment):
+        """
+        Return domain values for a variable
+        :param var:
+        :param assignment:
+        :return:domain
+        """
+        # We first Forward check and disable the domain variables that has already been assigned
+        for i in range(self.n):
+            if self.assignment[i] is None:
                 continue
-            if assignment[i]<=assignment[i-1]:
-                self.consistent_dict[tuple(assignment.items())] = False
+            value = assignment[i]
+            for j in range (self.n):
+                if j != i and self.domain[j][value]!=1:
+                    self.domain[j][value]=1
+
+        domain_values = []
+        for i in range(len(self.domain[var])):
+            if self.domain[var][i] != 1:
+                domain_values.append(i)
+        return domain_values
+
+
+    def consistency(self, marker, value, assignment):
+        """
+        This method performs the folloing consistency checks
+        1. X1<X2<X3<...Xn where Xi is the ith Marker value
+        2. Distance between every pair of marker is unique
+        :param marker:
+        :param value:
+        :param assignment:
+        :return: True if assignment of value to marker is consistent else False
+        """
+        temp_assignment = {}  # Dictionary to hold intermediate assignment
+        for i in range(len(assignment)):
+            if i == marker:
+                temp_assignment[i] = value  # Assign marker the value
+            else:
+                temp_assignment[i] = assignment[i]
+
+        # If this assignment is already part of consistenct assignment return its status
+        if tuple(temp_assignment.items()) in self.consistent_assignments:
+            return self.consistent_assignments[tuple(temp_assignment.items())]
+
+        # Checking constraint  1 of values in increasing order
+        for i in range(1, len(temp_assignment)):
+            if temp_assignment[i] == None or temp_assignment[i - 1] == None:
+                continue
+            if temp_assignment[i] <= temp_assignment[i - 1]:
+                # Previous marker value is greater that current marker assignment then this is an inconsistent assignment return False
+                self.consistent_assignments[tuple(temp_assignment.items())] = False
                 return False
 
         # Calculate all distance between assignment values
-        distance_list=[]
+        distance_list = []
         for i in range(self.n):
-            for j in range(self.n-1-i):
-                b=assignment[self.n-1-i]
-                a=assignment[(self.n-1-i)-(j+1)]
+            for j in range(self.n - 1 - i):
+                b = temp_assignment[self.n - 1 - i]
+                a = temp_assignment[(self.n - 1 - i) - (j + 1)]
 
                 if b is None or a is None:
                     continue
-                distance_list.append(b-a)
+                distance_list.append(b - a)  # Append all distances of every marker values
 
         distance_list.sort()
+        # If list has duplicate values means that distances are not unique and assignment is inconsistent
         if len(distance_list) > len(set(distance_list)):
-            self.consistent_dict[tuple(assignment.items())]=False
+            self.consistent_assignments[tuple(temp_assignment.items())] = False
             return False
-
-        if assignment[self.n-1] is not None and assignment[self.n-1]-assignment[0]<self.d-1:
+        # Check if the distance of last marker value and first marker value is less than length of ruler then return False
+        if temp_assignment[self.n - 1] is not None and temp_assignment[self.n - 1] - temp_assignment[0] < self.d - 1:
             return False
-
-        self.consistent_dict[tuple(assignment.items())]=True
+        # If none of the conistency checks are failed then return True
+        self.consistent_assignments[tuple(temp_assignment.items())] = True
         return True
 
-    def inference(self, var, value):
-        """
-        find out the inference for the new assignment
-        :param var:
-        :param value:
-        :returns: a list of (var, value) tuples
-        """
-        temp = set()
-        return temp
 
-    def deepcopy_domain(self, domain, other):
-        for i in range(len(other)):
-            for j in range(len(other[i])):
-                domain[i][j] = other[i][j]
-
-
-def recursive_BT(assignment,backtrack,count):
-    #count = count +1
-    #failure = -1,{}
+def recursive_BT(assignment,backtrack):
+    """
+    Recursive Backtrack function which picks up an unassigned variable and for every domain value checks the consistency and assigns it
+    and then does a recursive call to itself for the next variable assignment
+    :param assignment:
+    :param backtrack:
+    :return: optimal result
+    """
+    # If assignment is complete return it
     if backtrack.complete(assignment):
-        return assignment,count
-
+        return assignment
+    # Get the next unassigned variable
     var = backtrack.select_unassigned_variable()
+    # Get all its possible domain values
     domain_values = backtrack.order_domain_values(var,assignment)
-    for i in domain_values:
+    for i in domain_values: # Loop through the domain values
         if backtrack.consistency(var,i,assignment):
             assignment[var] = i
-            domain = [[1 for x in range(backtrack.d)] for x in range(backtrack.n)]
-            backtrack.deepcopy_domain(domain, backtrack.domain)
-            count = count + 1
-            result,count = recursive_BT(assignment,backtrack,count)
+            # Call the function again to backtrack again for current assignment
+            result= recursive_BT(assignment,backtrack)
             if result is not False:
-                return assignment,count
+                return assignment
             assignment[var]=None
-            backtrack.deepcopy_domain(backtrack.domain, domain)
+
+    return False
 
 
-    return False,count
+def recursive_FC(assignment,fc):
+    """
+    Recursive Forward Check function which picks up an unassigned variable and for every domain value checks the consistency and assigns it
+    and then does a recursive call to itself for the next variable assignment
+    :param assignment:
+    :param backtrack:
+    :return:optimal result
+    """
+    # If assignment is complete return it
+    if fc.complete(assignment):
+        return assignment
+    # Get the next unassigned variable
+    var = fc.select_unassigned_variable()
+    # Get all its possible domain values
+    domain_values = fc.order_domain_values(var,assignment)
+    for i in domain_values:# Loop through the domain values
+        if fc.consistency(var,i,assignment):
+            assignment[var] = i
+            # Maintain a copy of the domain before calling the function again to backtrack again for current assignment
+            domain = [[1 for x in range(fc.d)] for x in range(fc.n)]
+            fc.copy_domain(domain, fc.domain)
+            result = recursive_FC(assignment,fc)
+            if result is not False:
+                return assignment
+            assignment[var]=None
+            fc.copy_domain(fc.domain, domain) # Reassign domain back in case of False
 
-
-
+    return False
 
 #Your backtracking function implementation
 def BT(L, M):
     "*** YOUR CODE HERE ***"
-    count =0
-    result = -1,[]
-    optimalresult = -1,[]
+    """
+    Backtrack function finds the optimal length ruler for a given L and M
+    """
+    optimal_length = -1
+    optimalresult = []
+    # Loop till L is greater or equal to M
     while (L >= M):
-        print (L)
-        ruler=Golumb_Ruler(M,L)
+        ruler=Golomb_Ruler(L,M)
         backtrack=BackTracking(ruler)
-        result,count = recursive_BT(backtrack.assignment,backtrack,count)
-        if result is not False:
+        result = recursive_BT(backtrack.assignment,backtrack)
+        if result is not False: # If optimal result found store it and check for L =L-1
             optimalresult=result
             L=L-1
             continue
-        else:
-            return optimalresult,count
-    return optimalresult,count
+        else: # if result is False return the previous optimal result
+            if optimalresult:
+                optimal_length = optimalresult[M - 1] - optimalresult[0]
+                optimalresult = optimalresult.values()
+                return optimal_length, optimalresult
+            else:
+                return optimal_length,optimalresult
+    return optimal_length,optimalresult
 
 
 #Your backtracking+Forward checking function implementation
+#Your backtracking+Forward checking function implementation
 def FC(L, M):
     "*** YOUR CODE HERE ***"
-    return -1,[]
+    """
+    Forward Checking with Backtrack finds an optimal length ruler for given L and M
+    """
+    optimal_length=-1
+    optimalresult = []
+    # Loop till L is greater or equal to M
+    while (L >= M):
+        ruler = Golomb_Ruler(L, M)
+        fc = ForwardChecking(ruler)
+        result = recursive_FC(fc.assignment,fc)
+        if result is not False:# If optimal result found store it and check for L =L-1
+            optimalresult = result
+            L = L - 1
+            continue
+        else: # if result is False return the previous optimal result
+            if optimalresult:
+                optimal_length = optimalresult[M - 1] - optimalresult[0]
+                optimalresult = optimalresult.values()
+                return optimal_length, optimalresult
+            else:
+                return optimal_length,optimalresult
+    return optimal_length, optimalresult
+
 
 #Bonus: backtracking + constraint propagation
 def CP(L, M):
     "*** YOUR CODE HERE ***"
     return -1,[]
 
-
-print (BT(34,8))
+print BT(6,4)
+print FC(6,4)
